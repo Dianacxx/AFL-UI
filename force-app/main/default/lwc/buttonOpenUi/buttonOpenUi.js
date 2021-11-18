e44211d65b79628b5a6948afec774f966f7c82dd
CarolinaMercado <carolina@bricklane.ai>
2021-11-18 12:08:59 -0500

    Editing DataTable, Already added files in AFL CPQ showing UI and working with query apex, not API CPQ yet

--- force-app/main/default/lwc/buttonOpenUi/buttonOpenUi.js
+++ force-app/main/default/lwc/buttonOpenUi/buttonOpenUi.js
@@ -22,4 +22,3 @@ export default class ButtonOpenUi extends NavigationMixin(LightningElement) {
         });
     }
 }
-
--- force-app/main/default/lwc/qleDataTable/qleDataTable.js
+++ force-app/main/default/lwc/qleDataTable/qleDataTable.js
@@ -2,26 +2,43 @@ import { LightningElement, wire, api, track } from 'lwc';
 import printQuoteLines from '@salesforce/apex/QuoteInformation.printQuoteLines'
 
 const COLUMNS = [
-    { label: 'Name', fieldName: 'Name' },
+    { label: 'Name 1', fieldName: 'Name' },
     { label: 'SBQQ__ProductSubscriptionType__c', fieldName: 'SBQQ__ProductSubscriptionType__c' },
+    {type: 'button-icon',initialWidth: 34,typeAttributes:{iconName: 'action:description',name: 'More', variant:'brand'}},
+    {type: 'button-icon',initialWidth: 34,typeAttributes:{iconName: 'action:delete',name: 'Delete', variant:'brand'}}
+];
+const COLUMNS2 = [
+    { label: 'Name 2', fieldName: 'Name' },
+    { label: 'SBQQ__ProductSubscriptionType__c', fieldName: 'SBQQ__ProductSubscriptionType__c' },
+    {type: 'button-icon',initialWidth: 34,typeAttributes:{iconName: 'action:description',name: 'More', variant:'brand'}},
+    {type: 'button-icon',initialWidth: 34,typeAttributes:{iconName: 'action:delete',name: 'Delete', variant:'brand'}}
 ];
 
 
 export default class QleDataTable extends LightningElement {
     @api recordId;
-    columns = COLUMNS; 
+    //columns = COLUMNS; 
+    @track columns; 
     @track quoteLines = []; 
-    @track aux;
-    @track aux2; 
-    
+    @track aux; 
+    @track datos;
+    @track listQuoteLines = []; 
+    @track listQuoteLines2 = []; 
+    @track addVariable = 'Not Yet';
+    @track aux2 = 0; 
+
+
     @wire(printQuoteLines, { quoteId: '$recordId'})
     wiredSeatList({error,data}) {
         if (data) {
             for (let key in data) {
                this.quoteLines.push({value:data[key], key:key});
+               this.listQuoteLines = [...data[key]];
+               this.aux = key; 
+               //CHANGE OF COLUMNS DEPENDING DATA RETRIEVING
+               if (this.aux == 1){this.columns = COLUMNS;}
+               else {this.columns = COLUMNS2;} //Depending key, it shows, columns names. 
             }
-            //this.aux = this.quoteLines.values()(0);
-            //this.aux2 = this.aux[0];
 
         } else if (error) {
             window.console.log(error);
@@ -29,8 +46,15 @@ export default class QleDataTable extends LightningElement {
     }
 
     get datos(){
-        const valor = this.quoteLines.values(1);
-        
+        const valor = this.aux;
         return valor;
     }
+
+    addRow(){
+        this.listQuoteLines2 = [...[this.listQuoteLines[this.aux2]]];
+        this.aux2 += 1; 
+        this.addVariable = 'Added'; 
+    }
+
+
 }
